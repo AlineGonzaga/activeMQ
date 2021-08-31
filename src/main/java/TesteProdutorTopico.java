29bedbc70daa1c894870dec20f4e4c858d3f2db3
A. G <laingonzaga@hotmail.com>
2021-08-31 19:18:02 -0300

    commit initial

---
+++ src/main/java/TesteProdutorTopico.java
@@ -0,0 +1,28 @@
+import javax.jms.*;
+import javax.naming.InitialContext;
+
+public class TesteProdutorTopico {
+
+    public static void main(String[] args) throws Exception {
+
+        InitialContext context = new InitialContext();
+        ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+
+        Connection connection = factory.createConnection();
+        connection.start();
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+        Destination topico = (Destination) context.lookup("loja");
+
+
+        MessageProducer producer = session.createProducer(topico);
+
+        Message message = session.createTextMessage("<pedido><id>4324</id></pedido>");
+       // message.setBooleanProperty("ebook", false);
+        producer.send(message);
+
+        session.close();
+        connection.close();
+        context.close();
+    }
+}
